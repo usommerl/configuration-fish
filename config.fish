8c2e90654f9f7e2ed1db87d86574bba427e04d90
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-03-24 13:28:17 +0100

    Add preexec function that updates the terminal start directory

--- config.fish
+++ config.fish
@@ -1,4 +1,9 @@
 abbr -a -g vi nvim
+abbr -a -g h history
+
+function update_terminal_start_directory --on-event fish_preexec
+  echo (pwd) > $HOME/.terminal_start_directory
+end
 
 set fish_greeting
 starship init fish | source
