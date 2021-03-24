f45188e6c75e09d4e4ef10390faf357f268bd265
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2021-03-24 18:59:25 +0100

    Add abbreviation for previous directory

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 abbr -a -g g git
 abbr -a -g h history
 abbr -a -g vi nvim
+abbr -a -g - cd -
 
 function update_terminal_start_directory --on-event fish_preexec
   echo (pwd) > $HOME/.terminal_start_directory
