2afcd23a1aaa9204bf025feb1da1beb40d59e2c4
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-04-23 11:39:54 +0200

    Redirect X server output to /dev/null

--- config.fish
+++ config.fish
@@ -139,7 +139,7 @@ zoxide init fish | source && \
 
 if status --is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx -- -keeptty &>-
+    exec startx -- -keeptty &> /dev/null
   end
 end
 # }}}
